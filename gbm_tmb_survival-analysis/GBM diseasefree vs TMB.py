86876fd8b8e377bb10e62eef9f9fcedcb2b9cd05
fhd-bio <fhdalsaad.07@gmail.com>
2026-06-19 13:49:16 +0300

    Add files via upload

---
+++ gbm_tmb_survival-analysis/GBM diseasefree vs TMB.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('/gbm_tcga,cell_pub2013_clinical_data.csv')
+df = df.dropna(subset=['Disease Free (Months)', 'TMB (nonsynonymous)'])
+
+x = df['Disease Free (Months)']
+y = df['TMB (nonsynonymous)']
+plt.scatter(x, y)
+plt.xlabel('Disease Free (Months)')
+plt.ylabel('TMB (nonsynonymous)')
+plt.title('Glioblastoma Disease free status in months vs Total mutational burden (TBM)')
+plt.show()
